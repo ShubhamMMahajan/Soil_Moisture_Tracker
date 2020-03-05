e8f80b2391b6a579eb6f87ac88b4bb04b29fdb0a
Shubham Mahajan <shubhammahajan@gmail.com>
2020-03-05 13:18:31 -0800

    Deleted useless files

--- cloud-iot-gateways-rpi/start-gateway.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-export GOOGLE_CLOUD_PROJECT=soil-moisture-monitor-268204
-. env/Scripts/activate
-source run-gateway
-
-
--- raspberry_pi_code/seria_test.py
+++
@@ -1,9 +0,0 @@
-import serial
-import time
-ser = serial.Serial('/dev/ttyUSB0',9600)
-s = [0,1]
-while True:
-    read_serial=ser.readline()
-    serial_decoded = read_serial.decode("utf-8").strip()
-    print(serial_decoded)
-    time.sleep(1)
